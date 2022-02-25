24a50b3ab1c680950b1e0f449e6ba586cfd1f69d
Your Name <1192537447@qq.com>
2022-02-25 23:54:06 +0800

    init gshop

---
+++ README.md
@@ -0,0 +1,12 @@
+#vue-router
+2022年2月7日后vue-router默认版本改为4，4版本只能再vue3中使用，在vue2中使用vue3
+安装命令
+npm i vue-router@3
+
+vue2中stylus-loader版本为3
+安装命令
+npm i stylus-loader@3
+
+swiper轮播图不显示小圆点
+先卸载了swiper，安装swiper@5.4.5，引用时出现了css/swiper.in.css,但是运行时报错，然后卸载swiper@5.4.5，
+再执行npm i swiper，安装好后运行，就能出现小圆点。。。可能再原先基础上停掉项目再运行也行。或者是引用的css路径不对
---
+++ src/components/FooterGuide/FooterGuide.vue
@@ -0,0 +1,47 @@
+<template>
+  <footer class="footer_guide">
+
+    <router-link class="item_icon iconfont icon-waimai guide_item" active-class="on" to="/msite">外卖</router-link>
+    <router-link class="item_icon iconfont icon-search guide_item" active-class="on" to="/search">搜索</router-link>
+    <router-link class="item_icon iconfont icon-dingdan guide_item" active-class="on" to="/order">订单</router-link>
+    <router-link class="item_icon iconfont icon-geren guide_item" active-class="on" to="/profile">我的</router-link>
+
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "FooterGuide"
+}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+@import "../../common/stylus/mixins.styl"
+.footer_guide  //footer
+  top-border-1px(#e4e4e4)
+  position fixed
+  z-index 100
+  left 0
+  right 0
+  bottom 0
+  background-color #fff
+  width 100%
+  height 50px
+  display flex
+  .guide_item
+    display flex
+    flex 1
+    text-align center
+    flex-direction column
+    align-items center
+    margin 5px
+    color #999999
+    &.on
+      color #02a774
+    span
+      font-size 12px
+      margin-top 2px
+      margin-bottom 2px
+      .iconfont
+        font-size 22px
+</style>
