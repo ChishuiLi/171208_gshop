a35681a68cd3a03ef4fb6227eece33010a86f36b
ChishuiLi <1192537447@qq.com>
2022-02-27 17:12:16 +0800

    商家列表，vuex,登陆界面，提示界面等。。。

---
+++ src/store/actions.js
@@ -0,0 +1,59 @@
+/*通过mutations间接更新state的多个方法对象*/
+import {
+    reqPosition,
+    reqFoodCategorys,
+    reqShops
+} from '../api/index'
+import {
+    RECEIVE_ADDRESS,
+    RECEIVE_CATEGORYS,
+    RECEIVE_SHOPS,
+    /*RECEIVE_USER_INFO,
+    RESET_USER_INFO,
+
+    RECEIVE_SEARCH_SHOPS,
+
+    RECEIVE_GOODS,
+    RECEIVE_RATINGS,
+    RECEIVE_INFO,
+
+    DECREMENT_FOOD_COUNT,
+    INCREMENT_FOOD_COUNT,
+    CLEAR_CART,*/
+} from "./mutations-types"
+export default {
+    //异步获取地址
+    async getAddress({commit,state}){
+        //发送异步ajax请求
+        const geohash = state.latitude + ',' + state.longitude
+        const  result = await reqPosition(geohash)
+        //提交一个mutation
+        if(result.code === 0){
+            const  address = result.data
+            commit(RECEIVE_ADDRESS,{address})
+        }
+
+    },
+    //获取食品分类
+    async getFoodCategorys({commit}){
+        //发送异步ajax请求
+        const  result = await reqFoodCategorys()
+        //提交一个mutation
+        if(result.code === 0){
+            const categorys = result.data
+            commit(RECEIVE_CATEGORYS, {categorys})
+        }
+
+    },
+    async getShops({commit,state}){
+        //发送异步ajax请求
+        const {longitude, latitude} = state
+        const  result = await reqShops(longitude,latitude)
+        //提交一个mutation
+        if(result.code === 0){
+            const  shops = result.data
+            commit(RECEIVE_SHOPS,{shops})
+        }
+
+    }
+}
---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    devServer: {
+        proxy: {
+            '/api':{//匹配所有以'/api'开头的路径
+                target:'http://localhost:4000',//代理目标的基础路径
+                changeOrigin:true,
+                pathRewrite:{'^/api': ''}
+            }
+        }
+    },
+    lintOnSave: false
+}
